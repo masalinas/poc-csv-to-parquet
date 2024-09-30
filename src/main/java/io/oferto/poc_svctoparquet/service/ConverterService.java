f4c44c55e72088eef7b78db64c0ca1da5c3ca388
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2024-09-30 18:03:15 +0200

    last commit

--- src/main/java/io/oferto/poc_svctoparquet/service/ConverterService.java
+++ src/main/java/io/oferto/poc_svctoparquet/service/ConverterService.java
@@ -13,23 +13,31 @@ import org.springframework.web.multipart.MultipartFile;
 public class ConverterService {
 	private final String EXPORT_PATH = "/home/miguel/temp/";
 	
+	public static String removeExtension(String fname) {
+	      int pos = fname.lastIndexOf('.');
+	      if(pos > -1)
+	         return fname.substring(0, pos);
+	      else
+	         return fname;
+	   }
+	
     public String convert(MultipartFile file) throws IOException {
     	String tmpdir = System.getProperty("java.io.tmpdir");
         System.out.println("Temp file path: " + tmpdir);
 
-        // Create SparkSession
-        SparkSession spark = SparkSession.builder()
-                .appName("File Reader")
-                .master("local[*]") // Use local mode with all available cores
-                .getOrCreate();
-        
         // create a temporal file
-        Path tempFile = Files.createTempFile(null, null);
+        Path tempFile = Files.createTempFile(removeExtension(file.getOriginalFilename()), null);
         System.out.println(tempFile);
 
         // write csv file bytes array to the temporal one
         Files.write(tempFile, file.getBytes());
         
+        // Create SparkSession
+        SparkSession spark = SparkSession.builder()
+                .appName("File Reader")
+                .master("local[*]") // Use local mode with all available cores
+                .getOrCreate();
+                
         // read the CSV temporal file to Spark Dataframe
         Dataset<Row> fileData = spark.read().format("csv")
                 .option("header", "true")      // If CSV has a header
@@ -39,7 +47,7 @@ public class ConverterService {
         // write Spark Dataframe to parquet file
         fileData.write()
         	.format("parquet")
-        	.save(EXPORT_PATH + file.getName() + ".parquet");
+        	.save(EXPORT_PATH + removeExtension(file.getOriginalFilename()) + ".parquet");
             
         return "File convert successfully: " + file.getName() + ".parquet to " + EXPORT_PATH;
     }
