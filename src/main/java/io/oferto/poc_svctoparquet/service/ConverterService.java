b45c31bc43e56d426bce1a0a3eee342294ea5538
Miguel Angel Salinas Gancedo <masalinas.gancedo@gmail.com>
2024-10-05 14:18:40 +0200

    update to spark 3.x

--- src/main/java/io/oferto/poc_svctoparquet/service/ConverterService.java
+++ src/main/java/io/oferto/poc_svctoparquet/service/ConverterService.java
@@ -1,56 +1,41 @@
-package io.oferto.poc_svctoparquet.service;
+package io.oferto.poc_csvtoparquet.service;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ConverterService {
-	private final String EXPORT_PATH = "/home/miguel/temp/";
+	private final String EXPORT_PATH = "/Users/miguel/temp/";
 	
-	public static String removeExtension(String fname) {
-	      int pos = fname.lastIndexOf('.');
-	      if(pos > -1)
-	         return fname.substring(0, pos);
-	      else
-	         return fname;
-	   }
-	
-    public String convert(MultipartFile file) throws IOException {
-    	String tmpdir = System.getProperty("java.io.tmpdir");
-        System.out.println("Temp file path: " + tmpdir);
+    public String conver(MultipartFile file) throws IOException {
+    	Path tempFile = Files.createTempFile(null, null);
+    	Files.write(tempFile, file.getBytes());
+    			
+    	SparkSession spark = SparkSession.builder()
+    			  .appName("Java Spark csv to parquet poc")
+    			  .master("local[*]")
+    			  .config("spark.shuffle.blockTransferService", "nio")
+    			  .getOrCreate();
+    	
+    	Dataset<Row> df = spark.read().format("csv")
+    			.option("header", "true")
+    			.option("delimiter", ",")
+                .option("inferSchema", "true")
+                .load(tempFile.toString());
 
-        // create a temporal file
-        Path tempFile = Files.createTempFile(removeExtension(file.getOriginalFilename()), null);
-        tempFile.toFile().deleteOnExit();
-        
-        System.out.println(tempFile);
-
-        // write csv file bytes array to the temporal one
-        Files.write(tempFile, file.getBytes());
-        
-        // Create SparkSession
-        SparkSession spark = SparkSession.builder()
-                .appName("File Reader")
-                .master("local[*]") // Use local mode with all available cores
-                .getOrCreate();
-                
-        // read the CSV temporal file to Spark Dataframe
-        Dataset<Row> fileData = spark.read().format("csv")
-                .option("header", "true")      // If CSV has a header
-                .option("inferSchema", "true") // Infer data types
-                .load(tempFile.toString());   
-                 
-        // write Spark Dataframe to parquet file
-        fileData.write()
-        	.format("parquet")
-        	.save(EXPORT_PATH + removeExtension(file.getOriginalFilename()) + ".parquet");
-            
-        return "File convert successfully: " + file.getName() + ".parquet to " + EXPORT_PATH;
+    	df.write()
+    	  .format("parquet")
+    	  .save(EXPORT_PATH + file.getOriginalFilename() + ".parquet");
+    	  
+    	return "File convert successfully: " + file.getName() + ".parquet to " + EXPORT_PATH;
     }
 }
